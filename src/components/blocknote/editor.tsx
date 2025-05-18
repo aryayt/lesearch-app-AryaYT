a39a8f9036b85b860b9962c6dbd6efcffd260680
Ganesh Alla <allaganesh03@outlook.com>
2025-05-18 23:14:25 +0530

    theme

--- src/components/blocknote/editor.tsx
+++ src/components/blocknote/editor.tsx
@@ -2,12 +2,13 @@ import "@blocknote/core/fonts/inter.css";
 import { BlockNoteView } from "@blocknote/mantine";
 import "@blocknote/mantine/style.css";
 import { useCreateBlockNote } from "@blocknote/react";
- 
+import { useTheme } from "next-themes";
 export default function Editor() {
   // Creates a new editor instance.
   const editor = useCreateBlockNote();
+  const { resolvedTheme } = useTheme();
  
   // Renders the editor instance using a React component.
-  return <BlockNoteView editor={editor} />;
+  return <BlockNoteView editor={editor} theme={resolvedTheme === "dark" ? "dark" : "light"} />;
 }
  
